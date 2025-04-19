27fa22d3ee7dc291c51dde6583cfc3048112938e
Guillaume SCHNEIDER <guillaumeschneider@siggraph.org>
2025-04-19 21:13:36 +0200

    add: windows compatibility

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,13 +1,26 @@
 #include "utils.hpp"
+#include <iostream>
+
+
+#ifdef _WIN32
+#include <windows.h>
+#include <conio.h>
+#else
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <termios.h>
-#include <iostream>
+#endif
 
 int getTerminalHeight() {
+#ifdef _WIN32
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    return csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+#else
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
     return w.ws_row;
+#endif
 }
 
 void moveCursorToCommandLine() {
@@ -31,17 +44,33 @@ void clearLine() {
 }
 
 void enableRawMode() {
+#ifdef _WIN32
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hStdin, &mode);
+    mode &= ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT);
+    SetConsoleMode(hStdin, mode);
+#else
     termios term;
     tcgetattr(STDIN_FILENO, &term);
     term.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &term);
+#endif
 }
-
+    
 void disableRawMode() {
+#ifdef _WIN32
+    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD mode;
+    GetConsoleMode(hStdin, &mode);
+    mode |= (ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT);
+    SetConsoleMode(hStdin, mode);
+#else
     termios term;
     tcgetattr(STDIN_FILENO, &term);
     term.c_lflag |= (ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &term);
+#endif
 }
 
 std::string trim(const std::string& str) {
